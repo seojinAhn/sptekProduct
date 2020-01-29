cf0092fe435c8afcf022d2405bc358c4584cff82
안서진 <@host.docker.internal>
2020-01-29 14:35:21 +0900

    Test Update

--- MSA_Product/src/main/java/kr/co/sptek/product/HomeController.java
+++ MSA_Product/src/main/java/kr/co/sptek/product/HomeController.java
@@ -21,7 +21,7 @@ public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
-	 * Simply selects the home view to render by returning its name.
+	 * Simply selects the home view to render by returning its name. azure update sample
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, HttpSession session, Model model) {
